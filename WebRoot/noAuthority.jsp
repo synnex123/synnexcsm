0c3a058577638560d99be3f428628e9db16a4058
Joey <Joeyy@synnex.com>
2014-12-30 14:37:26 +0800

    checkAuthorityInterceptor

---
+++ WebRoot/noAuthority.jsp
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page isELIgnored="false" %>
+<%@ page import="java.util.Iterator" %>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script type="text/javascript" src="<%=request.getContextPath()%>/js/jquery-1.11.1.js"></script>
+<link rel="stylesheet" href="<%=request.getContextPath() %>/assets/lib/bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" href="<%=request.getContextPath() %>/assets/stylesheets_default/theme.css" />
+<link rel="stylesheet" href="<%=request.getContextPath() %>/assets/lib/font-awesome/css/font-awesome.css" />
+<script type="text/javascript" src="<%=request.getContextPath() %>/assets/lib/bootstrap/js/bootstrap-modal.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/assets/js/other.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/assets/js/jquery-ui.js"></script>
+<script type="text/javascript" src="<%=request.getContextPath() %>/assets/lib/bootstrap/js/bootstrap.js"></script>
+<title>菜单页面</title>
+</head>
+<body class="simple_body">
+	
+</body>
+权限不足
+</html>
---
+++ src/com/synnex/cms/interceptor/CheckAuthorityInterceptor.java
@@ -0,0 +1,68 @@
+package com.synnex.cms.interceptor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.synnex.cms.entity.User;
+
+public class CheckAuthorityInterceptor extends AbstractInterceptor{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String intercept(ActionInvocation actionInvocation) throws Exception{
+		List<String> managerActionName=new ArrayList<String>();
+		List<String> superManagerActionName=new ArrayList<String>();
+		List<String> userActionName=new ArrayList<String>();
+		String[] u={"UserChangePassword","InitUpdateUserInfo",
+				"UpdateUserInfo","Logout","UserSearch",
+				"initApply","applyManage","applyManagecancelApply",
+				"processApply","getMyApply","getApplyDetail",
+				"searchMyClub","init","exitClub","getPromotion",
+				"getPromotionVote","doPromotion"};
+		String[] m={"saveApply","rejectApply","getApplyByManagerId",
+				"producePromotion","initPromotion"};
+		String[] s={"AddClub","InitDeleteClub","DeleteClub"};
+		for(String i : u){
+			userActionName.add(i);
+		}
+		for(String i : m){
+			managerActionName.add(i);
+		}
+		for(String i : s){
+			superManagerActionName.add(i);
+		}
+		managerActionName.addAll(userActionName);
+		superManagerActionName.addAll(managerActionName);
+		String actionName=actionInvocation.getProxy().getActionName();
+		Map session=actionInvocation.getInvocationContext().getSession();
+		User user=(User)session.get("user");
+		if(user==null){
+			return actionInvocation.invoke();
+		}
+		else if(user.getUserType()==1){
+			if(userActionName.contains(actionName)){
+				return actionInvocation.invoke();
+			}
+			else return "noAuthority";
+		}
+		else if(user.getUserType()==0){
+			if(managerActionName.contains(actionName)){
+				return actionInvocation.invoke();
+			}
+			else return "noAuthority";
+			
+		}else if(user.getUserType()==10){
+			if(superManagerActionName.contains(actionName)){
+				return actionInvocation.invoke();
+			}
+			else return "noAuthority";
+		}else {
+			return "login_page";
+		}
+	}
+}
