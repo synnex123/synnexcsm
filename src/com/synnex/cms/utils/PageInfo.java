41672f695880e09f0f5ef8b340f6f0807958d928
petep <petep@cdc-d-petep.synnex.org>
2014-12-31 13:59:31 +0800

    asdfasf

---
+++ src/com/synnex/cms/utils/PageInfo.java
@@ -0,0 +1,26 @@
+package com.synnex.cms.utils;
+
+public class PageInfo {
+	private int currentPage;
+	private int totalPage;
+	private int pageRecords;
+	public int getCurrentPage() {
+		return currentPage;
+	}
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+	public int getTotalPage() {
+		return totalPage;
+	}
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+	public int getPageRecords() {
+		return pageRecords;
+	}
+	public void setPageRecords(int pageRecords) {
+		this.pageRecords = pageRecords;
+	}
+	
+}
